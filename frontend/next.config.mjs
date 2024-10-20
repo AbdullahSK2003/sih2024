45f71b04fcaa835cf75a342f435581fea69ee97d
AbdullahSK2003 <abdullah.sajid1974@gmail.com>
2024-10-21 00:06:47 +0530

    Abdullah

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // distDir: 'public', // Add this line
-    output: 'standalone', // Optional
   }
   
   module.exports = nextConfig
